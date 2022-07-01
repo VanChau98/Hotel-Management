834ccfe00465f36dc5ebbeb127595f18f04c5fd4
VanChauHuynh <48196528+VanChau98@users.noreply.github.com>
2022-07-01 21:51:25 +0700

    project

---
+++ ManagementHotel/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ManagementHotel.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ManagementHotel.ViewModels
+{
+    public class MainViewModel: BaseViewModel
+    {
+        public ICommand loginCommand { get; set; }
+
+        public MainViewModel()
+        {
+            loginCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                //ReceptionistWindow rw = new ReceptionistWindow();
+                //rw.ShowDialog();
+                ManagerWindow mw = new ManagerWindow();
+                mw.ShowDialog();
+            });
+        }
+
+       
+    }
+}
